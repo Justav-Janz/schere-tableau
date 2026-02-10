a89cfeb9a5a284b025812c93dc6be3588ffdbf1e
Justav-Janz <kohlen_weideland.61@icloud.com>
2026-02-10 23:12:10 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,8 @@
+Schere Tableau – PWA (Offline)
+
+1) Host den Dossier Inhalt iwwer HTTPS (z.B. GitHub Pages / Club-Site).
+2) Op iPhone: Safari -> Link opmaachen -> Share -> "Add to Home Screen".
+3) Du hues eng "App" um Homescreen (standalone). Offline geet no 1x Opmachen.
+
+WICHTEG:
+- Net iwwer file:/// opmaachen. Et muss iwwer https:// sinn, soss geet Service Worker/Offline net.
---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'schere-tableau-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then(cached => {
+      if (cached) return cached;
+      return fetch(req).then(res => {
+        const copy = res.clone();
+        if (req.method === 'GET' && new URL(req.url).origin === self.location.origin) {
+          caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
